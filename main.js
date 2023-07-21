4e13768aea315401007898f4e9776238f5010e20
Jiaming Guo <j23guo@gmail.com>
2023-07-21 19:58:19 -0400

    set up electron app

---
+++ main.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const url = require('url')
+
+let mainWindow
+
+function createWindow () {
+    mainWindow = new BrowserWindow({
+        width: 1440,
+        height: 900,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+    })
+
+    mainWindow.loadURL(
+        url.format({
+            pathname: path.join(__dirname, '/webapp/index.html'),
+            protocol: 'file:',
+            slashes: true
+        })
+    )
+
+    mainWindow.on('closed', () => {
+        mainWindow = null
+    })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow()
+    }
+})
